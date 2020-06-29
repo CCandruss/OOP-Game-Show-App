6a5fced5608c4d66957a6da1b81be62e998de16a
CCandruss <63433057+CCandruss@users.noreply.github.com>
2020-06-29 15:34:00 -0500

    off to the coffee shop
    next task step 8

--- ooop project/js/Game.js
+++ ooop project/js/Game.js
@@ -2,5 +2,44 @@
  * Project 4 - OOP Game App
  * Game.js */
 class Game{
-    
+    constructor(){
+        this.missed = 0;
+        this.phrases = [];
+        this.activePhrase = null;
+    }
+
+    createPhrases(phrase){
+        this.phrases.push(phrase);
+    }
+    getRandomPhrase(){
+        let j = Math.floor(Math.random()*this.phrases.length);
+        const phrase = new Phrase(this.phrases[j]);
+        return phrase;
+    }
+    startGame(){
+        const startScreen = document.getElementById('overlay');
+        startScreen.style.display = 'none';
+        const newPhrase = this.getRandomPhrase();
+        this.activePhrase = newPhrase;
+        this.activePhrase.addPhraseToDisplay(newPhrase);
+    }
 }
+const game = new Game();
+function plugInPhrases(){
+    game.createPhrases('going to hell in a hand basket');
+    game.createPhrases('rub a little dirt on it');
+    game.createPhrases('on a wild goose chase');
+    game.createPhrases('good things come to those who wait');
+    game.createPhrases('it is not rocket science');
+    game.createPhrases('bite the bullet');
+    game.createPhrases('aint nothin to it but to do it');
+    game.createPhrases('break a leg');
+    game.createPhrases('beat around the bush');
+    game.createPhrases('call it a day');
+    game.createPhrases('do or do not there is no try');
+};
+plugInPhrases();
+
+
+
+
--- ooop project/js/app.js
+++ ooop project/js/app.js
@@ -1,3 +1,8 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+//phrases hidden here
+
+game.startGame();
+console.log(`Active phrase - phrase: ${game.activePhrase.phrase}`);
+
